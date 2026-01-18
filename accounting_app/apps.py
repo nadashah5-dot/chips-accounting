2814309426f3cfecdda9535a709299ff39bf8fb8
Nada Shah <nadashah@gmail.com>
2026-01-18 13:21:53 +0300

    Seed accounts after migrations using post_migrate

--- accounting_app/apps.py
+++ accounting_app/apps.py
@@ -1,17 +1,14 @@
-# accounting_app/apps.py
-
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class AccountingAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "accounting_app"
 
     def ready(self):
-        # تشغيل seed مرة وحدة عند الإقلاع (لو الداتابيس فاضية)
-        try:
-            from .seed_accounts import seed_accounts_if_empty
+        from .seed_accounts import seed_accounts_if_empty
+
+        def run_seed(sender, **kwargs):
             seed_accounts_if_empty()
-        except Exception:
-            # ما بدنا نكسر تشغيل السيرفر لأي سبب
-            pass
+
+        post_migrate.connect(run_seed, sender=self)
